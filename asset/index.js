b5dbf4587c85062c9639a2c4dd48059cb864c2fa
wurui <wurui.wr@gmail.com>
2017-07-02 19:05:02 +0800

    first commit

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,7 @@
 define(['oxjs'],function(OXJS){
   return {
     init:function($mod){
-        $('.J_mainlink',$mod).attr('href',OXJS.queryString('qrcode_url'))
+        $('.J_mainlink',$mod).attr('href',decodeURIComponent(OXJS.queryString('qrcode_url')))
     }
   }
 })
